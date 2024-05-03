11b2a99589fc3d8188f6015c6a3c43c389659982
ibra <muhammadibrahimhisham@gmail.com>
2024-05-03 21:27:01 +0500

    draw screen

--- ilaichi-gui/src/main.rs
+++ ilaichi-gui/src/main.rs
@@ -6,13 +6,19 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+const TICKS_PER_FRAME: u32 = 10;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -53,7 +59,33 @@ fn main() {
                     _ => ()
                 }
         }
+        
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+
+        draw_screen(&chip8,&mut canvas);
     }
+}
+
+fn draw_screen(emu: &Emulator, canvas: &mut Canvas<Window>) { 
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    
+    let screen_buf = emu.get_display();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+            
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+
+    canvas.present(); 
 }
 
 fn emulate_keys(key: Keycode) -> Option<usize> {
